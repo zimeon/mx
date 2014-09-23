61813fba60bc68cccaaad3c7d2f08a4ffdef84f6
Simeon Warner <simeon.warner@cornell.edu>
2014-09-23 12:21:02 -0400

    Add --dupes option

--- mx_grep_oclc.py
+++ mx_grep_oclc.py
@@ -15,7 +15,10 @@ import optparse
 import logging
 
 class mx_grepper:
-    def __init__(self):
+    def __init__(self, show_dupes=False):
+        # Options
+        self.show_dupes=show_dupes
+        # Stats collected over run
         self.records_seen = 0
         self.records_matched = 0
         self.fields_matched = 0
@@ -40,12 +43,17 @@ class mx_grepper:
         """Look for one or more OCLC identifiers for record
 
         OCLC numbers are recorded in 035$a. The number itself should
-        be a decimal number. In some cases there are 'ocm' or 'OCM'
-        prefixes (what do they mean?). Usually these are duplicate
+        be a decimal number. In some cases there are 'ocm', 'ocn', or 
+        'OCM' prefixes (what do they mean?). Usually these are duplicate
         entries.
 
         What does it mean to have genuinely different numbers? e.g.
-        #4958095 ocm35304571 96047844
+        http://newcatalog.library.cornell.edu/catalog/302435
+        which has both OCLC nums 3615963 and 9860311 
+
+        What does it mean to have space separated data? I assume these
+        are error cases. e.g.
+        #4958095 'ocm35304571 96047844'
         """
         oclcnums = {}
         for f035 in record.get_fields('035'):
@@ -57,17 +65,19 @@ class mx_grepper:
                 if (m):
                     self.fields_matched += 1
                     entry = m.group(1)
-                    m2 = re.match(r'^(ocm|OCM)?(\d+)$',entry)
+                    m2 = re.match(r'^(ocm|ocn|OCM)?(\d+)$',entry)
                     if (m2):
                         ref = m2.group(2)
                         if (ref in oclcnums):
                             # dupe
                             self.fields_duped += 1
+                            if (self.show_dupes):
+                                logging.warning("[%s] Dupe 035$a OCLC value: '%s'",self.bibid,ref) 
                         else:
                             oclcnums[ref] = 1
                     else:
                         self.fields_bad += 1
-                        logging.warning("Bad 035$a OCLC entry in '%s': '%s'",self.bibid,entry)
+                        logging.warning("[%s] Bad 035$a OCLC entry: '%s'",self.bibid,entry)
         return sorted(oclcnums.keys())
 
 # Options and arguments
@@ -75,13 +85,15 @@ __version__ = '0.0.1'
 p = optparse.OptionParser(description='MARCXML Record Grepper -- currently just deals with the special case of looking for OCLC refs',
                           usage='usage: %prog [[opts]] [file1] .. [fileN]',
                           version='%prog '+__version__ )
+p.add_option('--dupes', action='store_true',
+              help="issue warnings for duplicate data")
 p.add_option('--verbose', '-v', action='store_true',
               help="verbose, show additional informational messages")
 (opt, args) = p.parse_args()
 
 # Loop over all files specified looking at each records
 files = 0
-mg = mx_grepper()
+mg = mx_grepper(show_dupes=opt.dupes)
 print "#bibid oclcnum[s]"
 for arg in args:
     fh = 0
